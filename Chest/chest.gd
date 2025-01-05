e1fb9b315458786d4aa36fe388157f8c71c61bea
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-01-06 01:10:37 +0530

    <Feat:Init chest feat opening and closing>

---
+++ Chest/chest.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+var state = "closed"
+var area_entered =false
+func _ready() -> void:
+	pass
+
+func _process(_delta):
+		if state=="closed":
+			$AnimatedSprite2D.play("close")
+			if  Input.is_action_just_pressed("open_chest") and area_entered:
+				state = "open"
+				await get_tree().create_timer(0.3).timeout
+
+		if state =="open":
+			$AnimatedSprite2D.play("open")
+			if  Input.is_action_just_pressed("open_chest") and area_entered:
+				state ="closed"
+				await get_tree().create_timer(0.3).timeout
+			if !area_entered:
+				state ="closed"
+				await get_tree().create_timer(0.3).timeout
+
+			
+		
+			
+	
+	
+	
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+			area_entered=true
+		
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+		if body.has_method("player"):
+			area_entered=false
+		
