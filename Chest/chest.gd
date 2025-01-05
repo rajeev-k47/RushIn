b832197d1d610898c2fcc7850434b814f515018c
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-01-06 02:39:56 +0530

    <Feat:Adds ui updation ,area based open closing chest>

--- Chest/chest.gd
+++ Chest/chest.gd
@@ -1,24 +1,30 @@
 extends Node2D
 
+
 var state = "closed"
 var area_entered =false
+
 func _ready() -> void:
-	pass
+	$Chest_inv.visible=false
+	
 
 func _process(_delta):
 		if state=="closed":
 			$AnimatedSprite2D.play("close")
 			if  Input.is_action_just_pressed("open_chest") and area_entered:
 				state = "open"
+				$Chest_inv.visible=true
 				await get_tree().create_timer(0.3).timeout
 
 		if state =="open":
 			$AnimatedSprite2D.play("open")
 			if  Input.is_action_just_pressed("open_chest") and area_entered:
 				state ="closed"
+				$Chest_inv.visible=false
 				await get_tree().create_timer(0.3).timeout
 			if !area_entered:
 				state ="closed"
+				$Chest_inv.visible=false
 				await get_tree().create_timer(0.3).timeout
 
 			
