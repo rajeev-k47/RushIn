aad84c1ef5899e1be5d96aa1e8ffec4ed6534cfb
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-01-25 00:02:23 +0530

    <Feat:Slime func>?

--- script/slime.gd
+++ script/slime.gd
@@ -3,10 +3,12 @@ extends CharacterBody2D
 
 var speed = 50
 var health  = 100
-var damage 
 var dead = false
 var player_in_area = false
 var player
+
+@onready var slime = $slime_collectable
+@export var item_Resource:Invitem
 
 func _ready():
 	dead = false
@@ -34,3 +36,45 @@ func _on_detection_area_body_entered(body: Node2D) -> void:
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	if body.has_method("player"):
 		player_in_area=false
+
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	var damage
+	if area.has_method("arrow_deal_damage"):
+		damage=50
+		take_damage(damage)
+
+func take_damage(damage):
+	health = health-damage
+	if health <=0 and !dead:
+		death()
+		
+func death():
+	dead = true
+	$AnimatedSprite2D.play("death")
+	await get_tree().create_timer(1).timeout
+	drop_slime()
+	
+	$AnimatedSprite2D.visible = false
+	$hitbox/CollisionShape2D.disabled = true
+	$detection_area/CollisionShape2D.disabled = true
+
+func drop_slime():
+	slime.visible = true
+	$slime_collect_area/CollisionShape2D.disabled=false
+	slime_collect()
+	
+func slime_collect():
+	await get_tree().create_timer(1.5).timeout
+	slime.visible = false
+	player.collect(item_Resource)
+	queue_free()
+	
+
+func _on_hitbox_area_exited(area: Area2D) -> void:
+	pass
+
+
+func _on_slime_collect_area_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player=body
