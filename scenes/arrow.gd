c56dd4e09dbbd04f031d19ffaac90333608f39c2
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-01-25 00:02:40 +0530

    <*/*>?

--- scenes/arrow.gd
+++ scenes/arrow.gd
@@ -8,7 +8,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position+= (Vector2.RIGHT*speed).rotated(rotation)*delta
-	
+
+func arrow_deal_damage():
+	pass
 
 
 func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
